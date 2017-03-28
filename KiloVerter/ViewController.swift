1623fe96a8021e71c50936fc7916ac9924d88bb9
Steve Tran <stran2252@gmail.com>
2017-03-28 13:46:30 -0700

    Initial Commit

---
+++ KiloVerter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KiloVerter
+//
+//  Created by Steve  Tran on 3/28/17.
+//  Copyright © 2017 Steve  Tran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
